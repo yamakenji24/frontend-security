947dc90a2a07a86a7a296592cb3b5f123ee8846c
yamakenji24 <yamakenji24@gmail.com>
2023-02-19 00:58:53 +0900

    chap5

--- chap5/server.ts
+++ chap5/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import https from "https";
 import fs from "fs";
+import crypto from "crypto";
 import api from "./routes/api";
 
 const app = express();
 const PORT = 80;
+
+app.set("view engine", "ejs");
 
 app.use((req, res, next) => {
   if (req.secure) {
@@ -12,15 +15,30 @@ app.use((req, res, next) => {
   } else {
     res.redirect(`https://${req.hostname}`);
   }
-})
+});
 
-app.use(express.static("public", {
-  setHeaders: (res, path, stat) => {
-    res.header("X-Frame-Options", "SAMEORIGIN");
-    res.header("Strict-Transport-Security", "max-age=60");
-  }
-}));
+app.use(
+  express.static("public", {
+    setHeaders: (res, path, stat) => {
+      res.header("X-Frame-Options", "SAMEORIGIN");
+      res.header("Strict-Transport-Security", "max-age=60");
+    },
+  })
+);
 app.use("/api", api);
+
+app.get("/csp", (req, res) => {
+  const nonceValue = crypto.randomBytes(16).toString("base64");
+
+  res.header(
+    "Content-Security-Policy", 
+    `script-src 'nonce-${nonceValue}' 'strict-dynamic';` +
+    "object-src 'none';" +
+    "base-uri 'none';" + 
+    "require-trusted-types-for 'script';"
+  );
+  res.render("csp", { nonce: nonceValue });
+});
 
 app.get("/", (req, res, next) => {
   res.end("Top Page");
